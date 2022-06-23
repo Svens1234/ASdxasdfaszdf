222559fc2ad99dd3f6d80627a40e1b28a58b0f0b
Svens1234 <88268560+Svens1234@users.noreply.github.com>
2022-06-23 18:28:54 +0300

    Delete main.sql

--- main.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE rations (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(20) NOT NULL,
-	weight INT DEFAULT 0,
-	color VARCHAR(20),
-	portion INT
-);
-ALTER TABLE rations RENAME TO portions;
